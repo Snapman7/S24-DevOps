ddfcbfafd8bbe719fc3fc2248d3d474deb6d5f59
Snapman7 <renat_ja02@mail.ru>
2024-02-27 22:38:41 +0300

    added lab3

---
+++ app_python/terraform/github/variables.tf
@@ -0,0 +1,6 @@
+variable "token" {
+  type        = string
+  description = "Specifies the GitHub PAT token or `GITHUB_TOKEN`"
+  sensitive   = true
+  default = "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"
+}
